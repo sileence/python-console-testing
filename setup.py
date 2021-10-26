a610431d6c346575d94709ea19c70570e31e80a7
Duilio Palacios <duilio@styde.net>
2021-10-26 16:35:19 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,16 @@
 """Setup script for realpython-reader"""
 
-# Standard library imports
-import pathlib
-
 # Third party imports
 from setuptools import setup
-
-# The directory containing this file
-HERE = pathlib.Path(__file__).resolve().parent
-
-# The text of the README file is used as a description
-README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
     name="console_testing",
-    version="0.1.0",
+    packages=["console_testing"],
+    version="0.1",
     description="Basic Python Package to help students write tests for programming exercises that interact with the console",
-    long_description=README,
-    long_description_content_type="text/markdown",
     url="https://github.com/sileence/python-console-testing",
+    keywords = ['Console', 'Testing'],
     author="Duilio Palacios",
     author_email="duilio@styde.net",
     license="MIT",
@@ -28,7 +19,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
     ],
-    packages=["console_testing"],
     include_package_data=True,
     install_requires=["", ""],
     entry_points={"console_scripts": [""]},
